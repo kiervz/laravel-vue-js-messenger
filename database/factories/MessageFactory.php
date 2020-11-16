4db00180eac3f1eeb68bf406e02ec04860072a7f
kiervz <kierveymaghanoy24@gmail.com>
2020-11-16 17:07:03 +0800

    Setup components

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MessageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Message::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'from' => function() {
+                return User::all()->random();
+            },
+            'to' => function() {
+                return User::all()->random();
+            },
+            'text' => $this->faker->sentence,
+        ];
+    }
+}
---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container-fluid">
+        <app-nav-bar></app-nav-bar>
+    </div>
+</template>
+
+<script>
+    import AppNavBar from './AppNavBar.vue'
+
+    export default {
+    components: { AppNavBar },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
